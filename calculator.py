4922c6582069b3df2a679cc9b35b07d81e8e1345
Seunzi <linxz2013@gmail.com>
2017-11-22 02:18:30 +0800

    Add files via upload
    Week 1 Challenge 2

--- calculator.py
+++ calculator.py
@@ -1,30 +1,49 @@
-#!/usr/bin/env python3
+#!/usr/bin/env/python3
 
 import sys
 
+def caltax(sal,itax,rate,dedu):
+    tax = itax * rate - dedu
+    osal = sal * (1-0.165) - tax
+    return(format(osal,".2f"))
 
-def caltax(salary,rate,deduct):
-    tex = format(salary * rate - deduct,".2f")
-    print(tex)
-
-if __name__ == '__main__':
-    try:
-        salary = int(sys.argv[1]) - 3500
-        if salary < 1500:
-            caltax(salary,0.03,0)
-        elif salary < 4500:
-            caltax(salary,0.10,105)
-        elif salary < 9000:
-            caltax(salary,0.20,555)
-        elif salary < 35000:
-            caltax(salary,0.25,1005)
-        elif salary < 55000:
-            caltax(salary,0.30,2755)
-        elif salary < 80000:
-            caltax(salary,0.35,5505)
-        elif salary > 80000:
-            caltax(salary,0.45,13505)
-    except ValueError:
-        print("Parameter Error")
-
+def calselect(sal):
+    itax = sal * (1 - 0.165) - 3500
+    if itax < 0:
+        itax = 0
+    
+    if sal < 3500:
+        return(sal)
+    elif itax <= 1500:
+        return(caltax(sal,itax,0.03,0))
+    elif itax <= 4500:
+        return(caltax(sal,itax,0.1,105))
+    elif itax <= 9000:
+        return(caltax(sal,itax,0.2,555))
+    elif itax <= 35000:
+        return(caltax(sal,itax,0.25,1005))
+    elif itax <= 55000:
+        return(caltax(sal,itax,0.30,2755))
+    elif itax <= 80000:
+        return(caltax(sal,itax,0.35,5505))
+    else:
+        return(caltax(sal,itax,0.45,13505))
  
+try:
+    idlist = []
+    taxlist = []
+   
+    for arg in sys.argv[1:]:
+        idsal = arg.split(':')
+        iid = int(idsal[0])
+        idlist.append(iid)
+        sal = int(idsal[1])
+        taxlist.append(calselect(sal))
+    
+    n = 0   
+    while n < len(idlist):
+        print("{}:{} ".format(idlist[n],taxlist[n]))
+        n += 1
+except ValueError:
+    print("Parameter Error")
+
