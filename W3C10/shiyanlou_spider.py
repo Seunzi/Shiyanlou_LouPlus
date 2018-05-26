706be1a26b1c4136d66435e41b688c55eaf99de0
Seunzi <linxz2013@gmail.com>
2018-05-26 23:53:20 +0800

    add W3C10

---
+++ W3C10/shiyanlou_spider.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+import scrapy
+
+class ShiyanlouSpider(scrapy.Spider):
+    name = 'github-repositories'
+
+    @property
+    def start_urls(self):
+        url_tmpl='https://github.com/shiyanlou?page={}&tab=repositories'
+        return (url_tmpl.format(i) for i in rage(1,5))
+
+    def parse(self,response):
+        for repository in response.xpath('.//div[@id="user-repositories-list"]/ul[@data-filterable-for="your-repos-filter"]/li'):
+            yield {
+                'name':repository.xpath('.//a[@itemprop="name codeRepository"]/text()').re_first('[\w\-\_]+')
+                'update_time': repository.xpath('.//div[@class="f6 text-gray mt-2"]/relative-time/@datetime').extract_first(default='Missing')
+            }
