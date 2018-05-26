683d3e42a5e94430b0906c1a44541e1547bbaeae
Seunzi <linxz2013@gmail.com>
2018-05-27 00:05:47 +0800

    fix some problems.

--- W3C10/shiyanlou_spider.py
+++ W3C10/shiyanlou_spider.py
@@ -7,11 +7,11 @@ class ShiyanlouSpider(scrapy.Spider):
     @property
     def start_urls(self):
         url_tmpl='https://github.com/shiyanlou?page={}&tab=repositories'
-        return (url_tmpl.format(i) for i in rage(1,5))
+        return (url_tmpl.format(i) for i in range(1,5))
 
     def parse(self,response):
         for repository in response.xpath('.//div[@id="user-repositories-list"]/ul[@data-filterable-for="your-repos-filter"]/li'):
             yield {
-                'name':repository.xpath('.//a[@itemprop="name codeRepository"]/text()').re_first('[\w\-\_]+')
+                'name': repository.xpath('.//a[@itemprop="name codeRepository"]/text()').re_first('[\w\-\_]+'),
                 'update_time': repository.xpath('.//div[@class="f6 text-gray mt-2"]/relative-time/@datetime').extract_first(default='Missing')
             }
