8e827a088693253887ef18a2a7b0cf788b5bd0e0
Seunzi <linxz2013@gmail.com>
2018-06-01 00:00:15 +0800

    fix some stupid problems

--- W3C12/shiyanlougithub/shiyanlougithub/spiders/multipage.py
+++ W3C12/shiyanlougithub/shiyanlougithub/spiders/multipage.py
@@ -10,12 +10,12 @@ class MultipageSpider(scrapy.Spider):
         return ('https://github.com/shiyanlou?page={}&tab=repositories'.format(i) for i in range(1,5))
 
     def parse(self, response):
-        for repository in response.xpath('.//div[@id="user-repositories-list"]/ul[@    data-filterable-for="your-repos-filter"]/li'):
+        for repository in response.xpath('.//div[@id="user-repositories-list"]/ul[@data-filterable-for="your-repos-filter"]/li'):
             item = ShiyanlougithubItem()
             item['name'] = repository.xpath('.//a[@itemprop="name codeRepository"]/text()').re_first('[\w\-\_]+')
             item['update_time'] = repository.xpath('.//div[@class="f6 text-gray mt-2"]/relative-time/@datetime').extract_first(default='Missing')
 
-            detail_url = response.urljoin(repository.xpath('.//a[@itemprop="name codeRepository"]/@href').extract_first()
+            detail_url = response.urljoin(repository.xpath('.//a[@itemprop="name codeRepository"]/@href').extract_first())
             request = scrapy.Request(detail_url,callback=self.parse_details)
             request.meta['item'] = item
             yield request
